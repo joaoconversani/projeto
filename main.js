f5b453955ea70cb16751302b7ee943a2cfa3f528
joaoconversani <joaoconversani@hotmail.com>
2024-05-28 20:18:08 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function addNewTask() {
+    var list = document.getElementById("list");
+    var text = document.getElementById("task_name").value;
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"; 
+
+    if(text.length === 0) {
+        alert("Tarefa precisa ter mais de um caracter");
+        return;
+    }
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
